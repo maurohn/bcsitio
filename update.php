483d0ce6368183a40e945a0df32963aecdb9b687
Mauro Novillo <mauro@mauronovillo.com.ar>
2017-01-23 20:20:39 +0000

    English tweaks and Blog added!

--- update.php
+++ update.php
@@ -1,31 +1,32 @@
 <?php
-if($_GET['user'] && $_GET['pass']) {
-
-$USER = $_GET['user'];
-$PASS = $_GET['pass'];
-$LOCAL_ROOT         = "/var/www/html/";
-//$LOCAL_REPO_NAME    = "REPO_NAME";
-//$LOCAL_REPO         = "{$LOCAL_ROOT}/{$LOCAL_REPO_NAME}";
-$AUTH               = "{$USER}:{$PASS}";
-$REMOTE_REPO        = "https://{$AUTH}@bitbucket.org/beatcoding/bc-site.git";
-$BRANCH             = "master";
-
-
+  if($_GET['user'] && $_GET['pass']) {
+    $USER = $_GET['user'];
+    $PASS = $_GET['pass'];
+    $LOCAL_ROOT         = "/var/www/html/";
+    //$LOCAL_REPO_NAME    = "REPO_NAME";
+    //$LOCAL_REPO         = "{$LOCAL_ROOT}/{$LOCAL_REPO_NAME}";
+    $AUTH               = "{$USER}:{$PASS}";
+    $REMOTE_REPO        = "https://{$AUTH}@bitbucket.org/beatcoding/bc-site.git";
+    $BRANCH             = "master";
+    
     // If there is already a repo, just run a git pull to grab the latest changes
     shell_exec("cd {$LOCAL_REPO} && git pull {$REMOTE_REPO} {$BRANCH}");
     die("Update done in " . mktime() ."ms");
-}else {
+  }else {
     echo "User or Pass Incorrect!";
-}
+  }
 ?>
 
 <html>
-<head>
-<title>Updat Bitbacket</title>
-</head>
-<form action="update.php" method = "get">
-<label for="username">Username</label> <input type="text" id="user" name="user"><br /><br />
-<label for="password">Password:</label> <input type="password" id="pass" name="pass"><br /><br />
-<button type = "submit">Send!</button>
-</form>
+  <head>
+    <title>Update Bitbucket</title>
+  </head>
+  
+  <body>
+    <form action="update.php" method = "get">
+      <label for="username">Username</label> <input type="text" id="user" name="user"><br /><br />
+      <label for="password">Password:</label> <input type="password" id="pass" name="pass"><br /><br />
+      <button type = "submit">Send!</button>
+    </form>
+  </body>
 </html>
